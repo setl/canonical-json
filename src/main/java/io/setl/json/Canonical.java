cabbc30df0d988d5ee64625f2a34d6567a7131f0
Simon Greatrix <simon.greatrix@setl.io>
2020-01-22 07:42:40 +0000

    end-of-day

--- src/main/java/io/setl/json/Canonical.java
+++ src/main/java/io/setl/json/Canonical.java
@@ -1,10 +1,19 @@
 package io.setl.json;
 
+import io.setl.json.primitive.PNumber;
+import io.setl.json.primitive.PString;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
+import javax.json.JsonArray;
+import javax.json.JsonNumber;
+import javax.json.JsonObject;
+import javax.json.JsonString;
+import javax.json.JsonStructure;
+import javax.json.JsonValue;
+import javax.json.JsonValue.ValueType;
 
 /**
  * Correctly present text and numeric data in the canonical form.
@@ -25,6 +34,92 @@ public class Canonical {
    */
   private static void appendUnicode(Appendable buf, char ch) throws IOException {
     buf.append("\\u").append(HEX[(ch >>> 12) & 15]).append(HEX[(ch >>> 8) & 15]).append(HEX[(ch >>> 4) & 15]).append(HEX[ch & 15]);
+  }
+
+
+  /**
+   * Convert any JsonValue into a canonical one.
+   *
+   * @param input JSON to convert
+   *
+   * @return the JSON as a canonical form
+   */
+  public static JsonValue convert(JsonValue input) {
+    return Primitive.create(input);
+  }
+
+
+  /**
+   * Convert any JsonArray into a canonical one.
+   *
+   * @param input JSON to convert
+   *
+   * @return the JSON as a canonical form
+   */
+  public static JsonArray convert(JsonArray input) {
+    if (input instanceof JArray) {
+      return input;
+    }
+    return JArray.fixCollection(input.asJsonArray());
+  }
+
+
+  /**
+   * Convert any JsonObject into a canonical one.
+   *
+   * @param input JSON to convert
+   *
+   * @return the JSON as a canonical form
+   */
+  public static JsonObject convert(JsonObject input) {
+    if (input instanceof JObject) {
+      return input;
+    }
+    return JObject.fixMap(input.asJsonObject());
+  }
+
+
+  /**
+   * Convert any JsonStructure into a canonical one.
+   *
+   * @param input JSON to convert
+   *
+   * @return the JSON as a canonical form
+   */
+  public static JsonStructure convert(JsonStructure input) {
+    return input.getValueType() == ValueType.OBJECT
+        ? convert((JsonObject) input)
+        : convert((JsonArray) input);
+  }
+
+
+  /**
+   * Convert any JsonValue into a canonical one.
+   *
+   * @param input JSON to convert
+   *
+   * @return the JSON as a canonical form
+   */
+  public static JsonNumber convert(JsonNumber input) {
+    if (input instanceof PNumber) {
+      return input;
+    }
+    return new PNumber(input.numberValue());
+  }
+
+
+  /**
+   * Convert any JsonValue into a canonical one.
+   *
+   * @param input JSON to convert
+   *
+   * @return the JSON as a canonical form
+   */
+  public static JsonString convert(JsonString input) {
+    if (input instanceof PString) {
+      return input;
+    }
+    return new PString(input.getString());
   }
 
 

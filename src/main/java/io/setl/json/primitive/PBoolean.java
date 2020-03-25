a70a5abf7209df47e6bfb22fa8dc5a3efd547e1b
Simon Greatrix <simon.greatrix@setl.io>
2020-03-25 10:54:43 +0000

    CBC-2039 : Extended marker interface to clarify that the type is a Boolean

--- src/main/java/io/setl/json/primitive/PBoolean.java
+++ src/main/java/io/setl/json/primitive/PBoolean.java
@@ -1,8 +1,17 @@
 package io.setl.json.primitive;
 
 /**
+ * Marker interface for the PTrue and PFalse primitives.
+ *
  * @author Valerio Trigari, 23/03/2020
  */
 public interface PBoolean {
-  // Marker interface for the PTrue and PFalse primitives.
+
+  /**
+   * Get the raw value of this primitive, which is always a Boolean.
+   *
+   * @return the boolean value of this
+   */
+  Boolean getValue();
+
 }
